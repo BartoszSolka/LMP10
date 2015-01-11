5ab2f2c3576341b867a2398deeb036310c3d4400
Bartosz Solka <solkab@len.iem.pw.edu.pl>
2015-01-11 19:55:18 +0100

    poprawa bledow

--- gsl/matrix.c
+++ gsl/matrix.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <gsl/gsl_linalg.h>
+#include <gsl/gsl_matrix.h>
 matrix_t * make_matrix (int rn, int cn){
 	matrix_t *matrix = malloc(sizeof(*matrix));
-	matrix->mat = gsl_matrix_alloc(x, y);
+	matrix->mat = gsl_matrix_alloc(rn, cn);
 	return matrix;
 }
 
@@ -52,7 +53,7 @@ read_matrix (FILE * in)
     return NULL;
   for (i = 0; i < rn; i++)
     for (j = 0; j < cn; j++)
-      if (fscanf (in, "%lf", x) != 1) {
+      if (fscanf (in, "%lf", &x) != 1) {
         free_matrix (new_mat);
         return NULL;
       }
@@ -66,13 +67,14 @@ read_matrix (FILE * in)
 void
 write_matrix (matrix_t * m, FILE * out)
 {
-	return gsl_matrix_fprintf(out,m->mat,%f);
+	gsl_matrix_fprintf(out,m->mat,"%f");
 }
 
-matrix_t *
-copy_matrix (matrix_t * s)
+matrix_t 
+*copy_matrix (matrix_t * s)
 {
-	matrix_t *new_matrix = malloc(*new_matrix);
+	matrix_t *new_matrix;
+	gsl_matrix_set_zero(new_matrix);
 	gsl_matrix_memcpy(new_matrix->mat,s->mat);
 	return new_matrix;
 }
@@ -82,7 +84,7 @@ transpose_matrix (matrix_t * s)
 {
   matrix_t *d = NULL;
   if (s != NULL)
-    d = make_matrix (s->mat->(int)size1, s->mat->(int)size2);
+    d = make_matrix((int)s->mat->size1, (int)s->mat->size2);
   gsl_matrix_memcpy(d->mat,s->mat);
   gsl_matrix_transpose(d);
   return d;
@@ -103,10 +105,10 @@ xchg_cols (matrix_t * m, int i, int j)
 matrix_t *
 mull_matrix (matrix_t * a, matrix_t * b)
 {
-  if (a == NULL || b == NULL || a->cn != b->rn)
+  if (a == NULL || b == NULL || a->mat->size1 != b->mat->size2)
     return NULL;
   else {
-    matrix_t *c = make_matrix (a->mat->(int)size1, b->mat->(int)size2);
+    matrix_t *c = make_matrix ((int)a->mat->size1, (int)b->mat->size2);
     gsl_matrix_mul_elements(c,b);
   return c;
   }
@@ -118,8 +120,8 @@ ge_matrix (matrix_t * a)
   matrix_t *c = copy_matrix (a);
   if (c != NULL) {
     int i, j, k;
-    int cn = c->mat->(int)size1;
-    int rn = c->mat->(int)size2;
+    int cn = (int)c->mat->size1;
+    int rn = (int)c->mat->size2;
     double *e = c->mat;
     for (k = 0; k < rn - 1; k++) {      /* eliminujemy (zerujemy) kolumnę nr k */
       for (i = k + 1; i < rn; i++) {    /* pętla po kolejnych
@@ -138,8 +140,8 @@ bs_matrix (matrix_t * a)
 {
   if (a != NULL) {
     int r, c, k;
-    int cn = a->mat->(int)size1;
-    int rn = a->mat->(int)size2;
+    int cn = (int)a->mat->size1;
+    int rn = (int)a->mat->size2;
     double *e = a->mat;
 
     for (k = rn; k < cn; k++) { /* pętla po prawych stronach */
