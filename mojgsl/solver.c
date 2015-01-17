aed57f4ae20323c1409d859ae0ffbf4dcb828e8f
Bartosz Solka <solkab@len.iem.pw.edu.pl>
2015-01-17 19:30:21 +0100

    napisanie wreszcie dzialajacej funkcji rozwiazujacej

--- mojgsl/solver.c
+++ mojgsl/solver.c
@@ -9,29 +9,29 @@ void
 pivot_ge_in_situ_matrix (matrix_t * c)
 {
   size_t i, j, k;
-  double *x, *y,*z,*f,*a;
+  double x, y,z,f,a;
   size_t cn = c->mat->size2;
   size_t rn = c->mat->size1;
   double d;
   for (k = 0; k < rn-1; k++) {        /* eliminujemy (zerujemy) kolumnę nr k */
 	size_t piv = k;
 	for (i = k + 1; i < rn; i++){
-	x=gsl_matrix_ptr(c->mat,i,k);
-	y=gsl_matrix_ptr(c->mat,piv,k);
-	if (fabs(*(x)) > fabs(*(y)))
+	x=gsl_matrix_get(c->mat,i,k);
+	y=gsl_matrix_get(c->mat,piv,k);
+	if (fabs(x) > fabs(y))
         piv = i;}
     if (piv != k) {             /* jeśli diag. nie jest pivtem - wymień wiersze */
       xchg_rows (c, piv, k);
     }
     for (i = k + 1; i < rn; i++) {   
       
-	x=gsl_matrix_ptr(c->mat,i,k);
-        f=gsl_matrix_ptr(c->mat,k,k);
-      d = *(x) / *(f);
+	x=gsl_matrix_get(c->mat,i,k);
+        f=gsl_matrix_get(c->mat,k,k);
+      d = x/ f;
       for (j = k; j < cn; j++){
-        z=gsl_matrix_ptr(c->mat,i,j);
-	a=gsl_matrix_ptr(c->mat,k,j);  
-	*z-=d* *(a);
+        z=gsl_matrix_get(c->mat,i,j);
+	a=gsl_matrix_get(c->mat,k,j);  
+	gsl_matrix_set(c->mat,i,j,z-(d*a));
       }
     }
   }
@@ -39,7 +39,7 @@ pivot_ge_in_situ_matrix (matrix_t * c)
 
 
 
-
+/*
 int
 piv_ge_solver (matrix_t * eqs)
 {
@@ -55,6 +55,28 @@ piv_ge_solver (matrix_t * eqs)
   else
     return 1;
 }
-
+*/
+	
+int
+piv_ge_solver (matrix_t *eqs)
+{
+	
+	int i=0,j=0;
+	gsl_vector *v=gsl_vector_alloc(eqs->mat->size1);
 	
 
+	for(i=0;i<eqs->mat->size1;i++)
+	  gsl_vector_set(v,i,gsl_matrix_get(eqs->mat,i,eqs->mat->size2-1));
+	  matrix_t *a=make_matrix(eqs->mat->size1,eqs->mat->size2-1);
+	for(j=0;j<eqs->mat->size1;j++)
+	  for(i=0;i<eqs->mat->size2-1;i++)
+	    gsl_matrix_set(a->mat,j,i,gsl_matrix_get(eqs->mat,j,i));
+	
+
+	gsl_linalg_HH_svx(a->mat,v);
+
+	for(i=0;i<eqs->mat->size1;i++)
+	  gsl_matrix_set(eqs->mat,i,eqs->mat->size2-1,gsl_vector_get(v,i));
+	
+	return 0;
+}
