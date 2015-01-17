965d0fe20a0271a023bb1eb5dc041ee272d92909
Bartosz Solka <solkab@len.iem.pw.edu.pl>
2015-01-17 03:27:03 +0100

    zabezpieczenie przed wielokrotna kompilacja kodu:

--- mojgsl/solver.h
+++ mojgsl/solver.h
@@ -1,12 +1,19 @@
+#ifndef SOLVER_H
+#define SOLVER_H
+
+
 #include <stdio.h>
 #include <gsl/gsl_linalg.h>
 #include "matrix.h"
 #include <gsl/gsl_matrix.h>
 #include <gsl/gsl_cblas.h>
 #include <math.h>
+
+
 void
 pivot_ge_in_situ_matrix (matrix_t * c);
 
 int
 piv_ge_solver (matrix_t * eqs);
 
+#endif
